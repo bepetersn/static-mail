75ab331b324a3d4d90a409c110f24b4c8fc6ae39
Brian Peterson <bepetersn@gmail.com>
2015-05-09 03:00:20 -0500

    Add a nice little module to the bad example to provide templating capabilities... this particular module is not too bad

---
+++ examples/bad/templating.py
@@ -0,0 +1,17 @@
+
+"""
+templating.py
+
+A small module that provides an interface compatible with Flask's render_template.
+
+"""
+
+from jinja2 import Environment, FileSystemLoader
+import os
+
+templates_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'emails')
+env = Environment(loader=FileSystemLoader(templates_dir))
+
+
+def render_template(template_name, **context):
+    return env.get_template(template_name).render(**context)
