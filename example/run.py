ecabdc92478416a5ad1e574ef4b6c5bb56a25663
Brian Peterson <bepetersn@gmail.com>
2015-05-08 22:50:40 -0500

    Add an example project that is helping to clarify the interface for static-mail

---
+++ example/run.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from __future__ import unicode_literals
+from db import session
+from db.models import User, Promotion
+from bootstrap import bootstrap_data
+from config import MailConfig
+from static_mail import StaticMail
+
+
+if __name__ == '__main__':
+
+    # do init
+    config = MailConfig()
+    mail = StaticMail(config)
+
+    # create bootstrap data
+    bootstrap_data()
+
+    # act like we know why we're emailing people
+    user = session.query(User).first()
+    new_promo = session.query(Promotion).first()
+
+    mail.send_email_by_name(
+        name='use_my_service',
+        recipients=[user.email],
+        context={
+            'contact': user,
+            'promo': new_promo
+        }
+    )
+
+
