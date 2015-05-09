28d8f8febc136b43b590a7554938b5ddc568d759
Brian Peterson <bepetersn@gmail.com>
2015-05-09 03:32:04 -0500

    Print what is passed into static_mail interface for clarity

--- static_mail/__init__.py
+++ static_mail/__init__.py
@@ -3,7 +3,8 @@
 class StaticMail(object):
 
     def __init__(self, config_object):
-        pass
+        print("Got mail server config info: ")
+        print(config_object.__config_store__)
 
     def send_email_by_name(self, name, recipients, context=None):
         print name
