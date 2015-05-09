f2923d7a4d846713f12cf86e2a0b8e81f56e3ab2
Brian Peterson <bepetersn@gmail.com>
2015-05-09 01:48:50 -0500

    separate static mail responsibilities into two interfaces

--- static_mail/__init__.py
+++ static_mail/__init__.py
@@ -22,14 +22,29 @@ class StaticMail(object):
             name, as defined in <EMAIL_DIR>/emails.yaml. Each is
             turned into a template and built with the given
             context as well.
-            3) The final subject, plain_text, and html are used
-            to send an email message. Flask-Mail's Message class
-            has a nice interface, we will probably make our
-            interface compatible with that to the extent possible.
 
         """
 
-        print name
-        print recipients
-        print context
+        print('build email with name: {}'.format(name))
+        print('use context: {}'.format(context))
+        return self
+
+        #subject = self.get_subject_somehow()
+        #html = self.get_html_somehow()
+        #plain_text = self.get_plain_text_somehow()
+        #send_message(recipients, subject, plain_text, html)
+
+    def send_message(self, recipients, subject, plain_text, html):
+        """
+        The final subject, plain_text, and html are used
+        to send an email message. Flask-Mail's Message class
+        has a nice interface, we will probably make our
+        interface compatible with that to the extent possible.
+
+        """
+
+        print('send to recipients: {}'.format(recipients))
+        print('with subject: {}'.format(subject))
+        print('with plain_text: {}'.format(plain_text))
+        print('with html: {}...'.format(html[:200]))
         return self
