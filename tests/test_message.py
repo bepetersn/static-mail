4a50db80dff2453d8f6f5389da95aa3d4d2791ab
Brian Peterson <bepetersn@gmail.com>
2015-12-01 14:54:01 -0700

    Don't re-test the template rendering functionality, use mocks instead

--- tests/test_message.py
+++ tests/test_message.py
@@ -1,27 +1,28 @@
 
 from unittest import TestCase
-from unittest.mock import Mock
-from jinja2 import Environment
-from templated_mail import SubTemplateLoader
+from unittest.mock import Mock, MagicMock
+from jinja2 import Template
 from templated_mail.message import Message
+
+
+TEMPLATES = {
+    'subject': 'Happy Thanksgiving, {{ user.name }}',
+    'body': 'Hello {{ user.name }}! Hope you and {{ pet.name }} have a good day.',
+    'html': 'See you later! Please order from us again at {{ location.name }}.'
+}
 
 
 class TestMessageTemplate(TestCase):
 
-    TEMPLATES = {
-        'subject': 'Happy Thanksgiving, {{ user.name }}',
-        'body': 'Hello {{ user.name }}! Hope you and {{ pet.name }} have a good day.',
-        'html': 'See you later! Please order from us again at {{ location.name }}.'
-    }
-
     def test_message_requires_all_three_sub_templates(self):
-        templates = self.TEMPLATES.copy()
+        templates = TEMPLATES.copy()
         templates.pop('subject')
         self.assertRaises(AssertionError, Message, Mock(), templates)
 
     def test_message_successfully_renders(self):
-        message = Message(Environment(loader=SubTemplateLoader('/not/under/test')),
-                          self.TEMPLATES)
+
+        env = MagicMock(get_template=lambda name: Template(TEMPLATES[name]))
+        message = Message(env, TEMPLATES)
         data = message.render(
             user=Mock(name='Brian'),
             pet=Mock(name='Horsey'),
